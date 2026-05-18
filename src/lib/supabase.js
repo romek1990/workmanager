c39734b318ef613978ef36fd8abb9af1bf2b43bb
romek1990 <39529711+romek1990@users.noreply.github.com>
2026-05-18 15:05:58 +0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://nwetajywazzpxkdknqsf.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_5a-3ZAXHrNto4disNZxIUQ_VWX4Vj7w'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53ZXRhanl3YXp6cHhrZGtucXNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwODQyNzAsImV4cCI6MjA5NDY2MDI3MH0.msA0e1suCIxm1IDb3ZwbyRCYA5FDoUg-EQOWkzeCcVk'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
